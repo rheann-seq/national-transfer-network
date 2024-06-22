4166f79ab7ebe770bfeef0260f31f7c1773c1bee
rheann-seq <rheann.seq@gmail.com>
2024-06-22 16:13:08 -0400

    ajax call to the upload_excel added

---
+++ ntn_app/static/ntn_app/js/upload_data.js
@@ -0,0 +1,45 @@
+// upload.js
+
+$(document).ready(function () {
+	function getCookie(name) {
+		let cookieValue = null;
+		if (document.cookie && document.cookie !== "") {
+			const cookies = document.cookie.split(";");
+			for (let i = 0; i < cookies.length; i++) {
+				const cookie = cookies[i].trim();
+				if (cookie.substring(0, name.length + 1) === name + "=") {
+					cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+					break;
+				}
+			}
+		}
+		return cookieValue;
+	}
+	const csrftoken = getCookie("csrftoken");
+
+	$("#uploadForm").on("submit", function (e) {
+		e.preventDefault();
+
+		var formData = new FormData();
+		var fileInput = document.getElementById("fileInput");
+		var file = fileInput.files[0];
+		formData.append("file", file);
+
+		$.ajax({
+			url: "http://127.0.0.1:8000/api/upload_excel/", // Replace with the actual URL of your ExcelUploadView
+			type: "POST",
+			data: formData,
+			processData: false,
+			contentType: false,
+			beforeSend: function (xhr, settings) {
+				xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			},
+			success: function (response) {
+				$("#message").html("<p>" + response.message + "</p>");
+			},
+			error: function (xhr, status, error) {
+				$("#message").html("<p>" + xhr.responseText + "</p>");
+			},
+		});
+	});
+});
