c658cb54b784ca706f3246e5be04c7259e133953
rheann-seq <rheann.seq@gmail.com>
2024-06-24 00:59:29 -0400

    equivalent_course in models changed by to foreign key relationship

--- ntn_app/models.py
+++ ntn_app/models.py
@@ -20,7 +20,8 @@ class Course(models.Model):
     university = models.ForeignKey(University, on_delete=models.CASCADE)
     # four_year_university = models.ForeignKey(University, related_name='university', on_delete=models.CASCADE, blank=True)
     # two_year_university = models.ForeignKey(University, related_name='community_college', on_delete=models.CASCADE, blank=True)
-    equivalent_course = models.ManyToManyField('self', blank=True)
+    # equivalent_course = models.ManyToManyField('self', blank=True)
+    equivalent_course = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
 
